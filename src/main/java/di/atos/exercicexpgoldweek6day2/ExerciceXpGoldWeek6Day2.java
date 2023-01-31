c49a3b7213fa76c58703ebb340a5b46a864c489d
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-01-31 15:59:00 +0000

    first commit

---
+++ src/main/java/di/atos/exercicexpgoldweek6day2/ExerciceXpGoldWeek6Day2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.exercicexpgoldweek6day2;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class ExerciceXpGoldWeek6Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
